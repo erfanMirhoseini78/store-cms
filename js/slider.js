aceb324957751f54b7a1348c7b25500841955aa7
erfan <erfanmir696969@gmail.com>
2023-10-03 22:24:58 +0330

    feat: Create Article Section - Resolve #7

--- js/slider.js
+++ js/slider.js
@@ -11,7 +11,7 @@ const swiper = new Swiper('.swiper', {
         },
         1200: {
             slidesPerView: 3,
-            spaceBetween: 30,
+            spaceBetween: 15,
         }
     }
 })
