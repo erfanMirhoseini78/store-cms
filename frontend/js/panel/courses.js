1304aaa021bfaf01d2eb5f9b01fca0027a110ebb
erfan <erfanmir696969@gmail.com>
2023-10-21 15:20:23 +0330

    Update Show All Courses in Admin Panel

--- frontend/js/panel/courses.js
+++ frontend/js/panel/courses.js
@@ -2,48 +2,6 @@ import {
     getAllCourses,
 } from "./funcs/courses.js";
 
-const tableBodyWrapper = document.querySelector('.table-body__wrapper');
-
 window.addEventListener('load', () => {
-    tableBodyWrapper.innerHTML = "";
-
-    let count = 0;
-    getAllCourses()
-        .then(courses => {
-            console.log(courses);
-            courses.forEach(course => {
-                tableBodyWrapper.insertAdjacentHTML('beforeend', `
-                    <tr>
-                        <td>
-                            <input type="checkbox" class="checkbox-table form-check-input">
-                        </td>
-                        <td>
-                            ${++count}
-                        </td>
-                        <td id="name">
-                            <a href="file:///D:/Project/Admin_Panel/Product/index.html?id=${course._id}">
-                                ${course.name}
-                            </a>
-                        </td>
-                        <td>
-                            ${course.creator}
-                        </td>
-                        <td id="condition">
-                            ${course.registers}
-                        </td>
-                        <td id="price">
-                            ${course.support}
-                        </td>
-                        <td id="price">
-                            ${course.price ? `${course.price} تومان` : 'رایگان'}
-                        </td>
-                        <td>
-                            <button type="button" class="btn btn-primary" id="edit-btn">ویرایش</button>
-                        </td>
-                        <td>
-                            <button type="button" class="btn btn-danger" id="delete-btn">حذف</button>
-                        </td>
-                    </tr>`)
-            })
-        })
+    getAllCourses();
 })
--- frontend/js/panel/funcs/courses.js
+++ frontend/js/panel/funcs/courses.js
@@ -1,6 +1,53 @@
 const getAllCourses = async () => {
+    const tableBodyWrapper = document.querySelector('.table-body__wrapper');
+
     const res = await fetch('http://localhost:4000/v1/courses');
     const courses = await res.json();
+
+    tableBodyWrapper.innerHTML = "";
+    console.log(courses);
+
+    courses.forEach((course, index) => {
+        tableBodyWrapper.insertAdjacentHTML('beforeend', `
+            <tr>
+                <td>
+                    ${index + 1}
+                </td>
+                <td>
+                    ${course.name}
+                </td>
+                <td>
+                    ${course.creator}
+                </td>
+                <td>
+                    ${course.registers}
+                </td>
+                <td>
+                    ${course.support}
+                </td>
+                <td>
+                    ${course.price ? `${course.price} تومان` : 'رایگان'}
+                </td>
+                <td>
+                    ${course.isComplete ? 'به اتمام رسیده' : 'در حال برگزاری'}
+                </td>
+                <td class="course-score">
+                
+                    ${Array(5 - course.courseAverageScore).fill(0).map(score => '<img src="./../../images/svgs/star.svg" alt="Rating" class="course-box__star">').join('')}
+                    ${Array(course.courseAverageScore).fill(0).map(score => '<img src="./../../images/svgs/star_fill.svg" alt="Rating" class="course-box__star-fill">').join('')}
+
+                </td>
+                <td>
+                    ${course.categoryID.title}
+                </td>
+                <td>
+                    <button type="button" class="btn btn-primary" id="edit-btn">ویرایش</button>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-danger" id="delete-btn">حذف</button>
+                </td>
+            </tr>`)
+    })
 
     return courses;
 }
