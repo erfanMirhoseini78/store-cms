70c77b828030a52612b8f8f7be8f7f3d5c38038d
erfan <erfanmir696969@gmail.com>
2023-10-22 13:29:37 +0330

    Development Logic Create New Course

--- frontend/js/panel/courses.js
+++ frontend/js/panel/courses.js
@@ -1,10 +1,25 @@
 import {
     getAllCourses,
     removeCourse,
+    getCategory,
+    selectCover,
+    getStatus,
+    createNewCourse,
 } from "./funcs/courses.js";
 
 window.removeCourse = removeCourse;
 
+const submitBtnInput = document.querySelector('#submit-btn__input');
+
 window.addEventListener('load', () => {
     getAllCourses();
+    getCategory();
+    selectCover();
+    getStatus();
+
+    submitBtnInput.addEventListener('click', event => {
+        event.preventDefault();
+
+        createNewCourse();
+    })
 })
