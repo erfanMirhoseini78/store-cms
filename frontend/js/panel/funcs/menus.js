7747909e5cc27e38e624f77d1e1b022e097a495c
erfan <erfanmir696969@gmail.com>
2023-10-23 15:39:55 +0330

    Development Logic Create Menu && Sub Menu in Admin Panel

--- frontend/js/panel/funcs/menus.js
+++ frontend/js/panel/funcs/menus.js
@@ -1,3 +1,10 @@
+import {
+    getToken,
+    showSwal,
+} from "../../funcs/utility.js";
+
+let menuID = null; // undefined 
+
 const getAndShowAllMenus = async () => {
     const tableBody = document.querySelector('.table-body');
 
@@ -5,7 +12,6 @@ const getAndShowAllMenus = async () => {
     const menus = await res.json();
 
     tableBody.innerHTML = "";
-    console.log(menus);
     menus.forEach((menu, index) => {
         tableBody.insertAdjacentHTML('beforeend', `
             <tr>
@@ -35,6 +41,77 @@ const getAndShowAllMenus = async () => {
     return menus;
 }
 
+const prepareCreateNewMenu = async () => {
+    const selectElem = document.querySelector('.select');
+
+    const res = await fetch('http://localhost:4000/v1/menus');
+    const menusParent = await res.json();
+
+    menusParent.forEach(menu => {
+        selectElem.insertAdjacentHTML('beforeend', `
+            <option value="${menu._id}">
+                ${menu.title}
+            </option>
+        `)
+    })
+
+    selectElem.addEventListener('change', event => {
+        menuID = event.target.value;
+    })
+
+    return menusParent;
+}
+
+const createNewMenu = async () => {
+    let adminToken = getToken();
+
+    let selectElem = document.querySelector('.select');
+    const titleElem = document.querySelector('#title');
+    const hrefElem = document.querySelector('#href');
+
+    const newMenuInfos = {
+        title: titleElem.value.trim(),
+        href: hrefElem.value.trim(),
+        parent: menuID,
+    }
+
+    const res = await fetch('http://localhost:4000/v1/menus', {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${adminToken}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newMenuInfos)
+    })
+    const menu = await res.json();
+
+    if (res.status === 201) {
+        showSwal(
+            'success',
+            'منو با موفقیت ایجاد شد',
+            'هوراااااا',
+            () => {
+                getAndShowAllMenus();
+                titleElem.value = "";
+                hrefElem.value = "";
+                selectElem.firstElementChild.setAttribute('selected', 'selected');
+            }
+        )
+    }
+    else {
+        showSwal(
+            'error',
+            'اطلاعات مورد نیاز برای ایجاد منو رو به درستی وارد کنید',
+            'یه بار دیگه شانستو امتحان کن',
+            () => { }
+        )
+    }
+
+    return menu;
+}
+
 export {
     getAndShowAllMenus,
+    prepareCreateNewMenu,
+    createNewMenu,
 }
--- frontend/js/panel/menus.js
+++ frontend/js/panel/menus.js
@@ -1,7 +1,17 @@
 import {
     getAndShowAllMenus,
+    prepareCreateNewMenu,
+    createNewMenu,
 } from "./funcs/menus.js"
 
 window.addEventListener('load', () => {
+    const submitInputBtn = document.querySelector('#submit-input__btn');
+
     getAndShowAllMenus();
+    prepareCreateNewMenu();
+
+    submitInputBtn.addEventListener('click', event => {
+        event.preventDefault();
+        createNewMenu();
+    })
 })
