062b2a9993b75e8f6bdb1f756a769517758c2053
erfan <erfanmir696969@gmail.com>
2023-11-13 23:28:05 +0330

    Development Create Article in Article Page in Project

--- frontend/js/panel/articles.js
+++ frontend/js/panel/articles.js
@@ -1,10 +1,20 @@
 import {
     getAndShowArticles,
     removeArticle,
+    createNewArticle,
+    prepareCreateNewArticles,
 } from "./funcs/articles.js";
 
 window.removeArticle = removeArticle;
 
+const submitInputBtn = document.querySelector('#submit-input__btn');
+
 window.addEventListener('load', () => {
+    prepareCreateNewArticles();
     getAndShowArticles();
+})
+
+submitInputBtn.addEventListener('click', event => {
+    event.preventDefault();
+    createNewArticle();
 })
--- frontend/js/panel/funcs/articles.js
+++ frontend/js/panel/funcs/articles.js
@@ -3,6 +3,17 @@ import {
     showSwal,
 } from "../../funcs/utility.js";
 
+const categoryList = document.querySelector('.category-list');
+const fileElem = document.getElementById('file');
+const titleElem = document.querySelector('#title');
+const linkElem = document.querySelector('#link');
+const summaryElem = document.querySelector('#summary');
+const editorElem = document.querySelector('#editor');
+
+let categoryID = '-1';
+let coverArticle = null;
+let articleBodyEditor = null;
+
 const getAndShowArticles = async () => {
     const adminToken = getToken();
 
@@ -51,7 +62,88 @@ const removeArticle = articleID => {
     console.log(articleID);
 }
 
+const prepareCreateNewArticles = async () => {
+    // CKEditor
+    ClassicEditor
+        .create(document.querySelector('#editor'), {
+            placeholder: 'محتوای مقاله را وارد کنید ... !',
+            language: 'fa',
+        })
+        .then(result => {
+            articleBodyEditor = result;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    const res = await fetch(`http://localhost:4000/v1/category`);
+    const categories = await res.json();
+
+    categories.forEach(category => {
+        categoryList.insertAdjacentHTML('beforeend', `
+            <option value="${category._id}">
+                ${category.title}
+            </option>
+        `)
+    })
+
+    categoryList.addEventListener('change', event => categoryID = event.target.value);
+    fileElem.addEventListener('change', event => coverArticle = event.target.files[0]);
+}
+
+const createNewArticle = async () => {
+    const adminToken = getToken();
+
+    let formDate = new FormData();
+
+    formDate.append('title', titleElem.value.trim());
+    formDate.append('description', summaryElem.value.trim());
+    formDate.append('body', articleBodyEditor.getData());
+    formDate.append('shortName', linkElem.value.trim());
+    formDate.append('categoryID', categoryID);
+    formDate.append('cover', coverArticle);
+
+    const res = await fetch('http://localhost:4000/v1/articles', {
+        method: "POST",
+        headers: {
+            // 'Content-Type': 'application/json',
+            Authorization: `Bearer ${adminToken}`
+        },
+        body: formDate
+    })
+    const result = await res.json();
+
+    if (res.status === 201) {
+        showSwal(
+            'success',
+            'مقاله با موفقیت ایجاد شد',
+            'ایول',
+            () => {
+                titleElem.value = "";
+                summaryElem.value = "";
+                linkElem.value = "";
+                articleBodyEditor.setData('');
+                fileElem.value = "";
+                categoryList.value = '-1';
+                getAndShowArticles();
+            }
+        )
+    }
+    else {
+        showSwal(
+            'error',
+            'در ایجاد مقاله مشکلی به وجود آمده است',
+            'درستش میکنیم',
+            () => { }
+        )
+    }
+
+    return result;
+}
+
 export {
     getAndShowArticles,
     removeArticle,
+    createNewArticle,
+    prepareCreateNewArticles,
 }
